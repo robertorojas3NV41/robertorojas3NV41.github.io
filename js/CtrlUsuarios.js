00bebe0e68a30a4959f9f2a7a14a7b6e1260baa1
robertorojas3NV41 <86042594+robertorojas3NV41@users.noreply.github.com>
2021-06-19 02:02:03 -0500

    Update CtrlUsuarios.js

--- js/CtrlUsuarios.js
+++ js/CtrlUsuarios.js
@@ -19,8 +19,6 @@ const lista = document.
 const firestore = getFirestore();
 const daoRol = firestore.
   collection("Rol");
-const daoPasatiempo = firestore.
-  collection("Pasatiempo");
 const daoUsuario = firestore.
   collection("Usuario");
 
@@ -81,9 +79,6 @@ async function htmlFila(doc) {
   const data = doc.data();
   const img = cod(
     await urlStorage(doc.id));
-  const pasatiempo =
-    await buscaPasatiempo(
-      data.pasatiempoId);
   const roles =
     await buscaRoles(data.rolIds);
   const parámetros =
@@ -105,35 +100,12 @@ async function htmlFila(doc) {
           </strong>
           <span
               class="secundario">
-            ${pasatiempo}<br>
+            <br>
             ${roles}
           </span>
         </span>
       </a>
     </li>`);
-}
-
-/** Recupera el html de un
- * pasatiempo en base a su id.
- * @param {string} id */
-async function
-  buscaPasatiempo(id) {
-  if (id) {
-    const doc =
-      await daoPasatiempo.
-        doc(id).
-        get();
-    if (doc.exists) {
-      /**
-       * @type {import(
-          "./tipos.js").
-            Pasatiempo} */
-      const data = doc.data();
-      return (/* html */
-        `${cod(data.nombre)}`);
-    }
-  }
-  return "-- Sin Pasatiempo --";
 }
 
 /** Recupera el html de los
