c75967b1ffa25d388f99b3167d67301015db88d0
robertorojas3NV41 <86042594+robertorojas3NV41@users.noreply.github.com>
2021-06-18 22:44:39 -0500

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyCz50nLV4eZc47S8pqSCxSOaJROSJ7Ezg8",
+    authDomain: "fbwebauth41.firebaseapp.com",
+    projectId: "fbwebauth41",
+    storageBucket: "fbwebauth41.appspot.com",
+    messagingSenderId: "405306627572",
+    appId: "1:405306627572:web:a01e44d1801333eaa695a1",
+    measurementId: "G-XPRC00KTSR"
+  });
