f6355f1203a3a1320d9e67e87ea286396edd51b7
robertorojas3NV41 <86042594+robertorojas3NV41@users.noreply.github.com>
2021-06-19 21:25:50 -0500

    Update CtrlArticulos.js

--- js/CtrlArticulos.js
+++ js/CtrlArticulos.js
@@ -4,168 +4,85 @@ import {
 } from "../lib/fabrica.js";
 import {
   cod,
-  getString,
   muestraError
 } from "../lib/util.js";
 import {
   tieneRol
 } from "./seguridad.js";
 
-const daoMensaje = getFirestore().
-  collection("Mensaje");
-const daoArticulos = getFirestore().
-  collection("Articulos");
-let usuarioId = "";
-/** @type {HTMLFormElement} */
-const forma = document["forma"];
 /** @type {HTMLUListElement} */
 const lista = document.
   querySelector("#lista");
+const daoPasatiempo =
+  getFirestore().
+    collection("Pasatiempo");
 
-getAuth().onAuthStateChanged(
-  protege, muestraError);
+getAuth().
+  onAuthStateChanged(
+    protege, muestraError);
 
 /** @param {import(
     "../lib/tiposFire.js").User}
     usuario */
 async function protege(usuario) {
   if (tieneRol(usuario,
-    ["Cliente"])) {
-    usuarioId = usuario.email;
+    ["Administrador"])) {
     consulta();
-    forma.addEventListener(
-      "submit", agrega);
   }
 }
 
-/** Agrega un usuario a la base de
- * datos.
- * @param {Event} evt */
-async function agrega(evt) {
-  try {
-    evt.preventDefault();
-    const formData =
-      new FormData(forma);
-    /** @type {string} */
-    const nombre = getString(
-      formData, "nombre").trim();
-    const precio = getString(
-      formData, "precio").trim();
-    const descripcion = getString(
-      formData, "descripcion").trim();
-    /** @type {import(
-        "./tipos.js").Mensaje} */
-    const modelo = {
-      descripcion,
-      nombre,
-      precio
-    };
-    /* El modelo se agrega a
-     * la colección
-     * "Mensaje". */
-    await daoArticulos.add(modelo);
-    forma.texto.value = "";
-  } catch (e) {
-    muestraError(e);
-  }
-}
-
-/** Muestra los mensajes
- * almacenados en la collection
- * "Mensaje". Se actualiza
- * automáticamente. */
 function consulta() {
-  /* Consulta que se actualiza
-   * automáticamente. Pide todos
-   * los registros de la colección
-   *  "Mensaje"
-   * ordenados por el campo
-   *  "timestamp"
-   * de forma
-   *  descendente. */
-  daoArticulos.
-    onSnapshot(
+  daoPasatiempo.
+    orderBy("nombre")
+    .onSnapshot(
       htmlLista, errConsulta);
 }
 
-/** Muestra los datos enviados por
- * el servidor.
- * Si los datos cambian en el
- * servidor, se vuelve a invocar
- * esta función y recibe los datos
- * actualizados.
+/**
  * @param {import(
     "../lib/tiposFire.js").
-    QuerySnapshot} snap estructura
- *    parecida a un Array, que
- *    contiene una copia de los
- *    datos del servidor.
- */
+    QuerySnapshot} snap */
 function htmlLista(snap) {
   let html = "";
   if (snap.size > 0) {
-    /* Cuando el número de
-     * documentos devueltos por la
-     * consulta es mayor que 0,
-     * revisa uno por uno los
-     * documentos de la consulta y
-     * los muestra. El iterador
-     * "doc" apunta a un
-     * documento de la base
-     * de datos. */
     snap.forEach(doc =>
       html += htmlFila(doc));
   } else {
-    /* Cuando el número de
-     * documentos devueltos por la
-     * consulta es igual a 0,
-     * agrega un texto HTML. */
     html += /* html */
       `<li class="vacio">
-        -- No hay mensajes
+        -- No hay pasatiempos
         registrados. --
       </li>`;
   }
   lista.innerHTML = html;
 }
 
-/** Agrega el texto HTML
- * que corresponde a un
- * documento de un mensaje.
+/**
  * @param {import(
     "../lib/tiposFire.js").
     DocumentSnapshot} doc */
 function htmlFila(doc) {
-  /** Recupera los datos del
-   * documento.
+  /**
    * @type {import("./tipos.js").
-                      Mensaje} */
+                  Pasatiempo} */
   const data = doc.data();
-  /* Agrega un li con los datos
-   * del documento, los cuales se
-   * codifican para evitar
-   * inyección de código. */
+  const nombre = cod(data.nombre);
+  const parámetros =
+    new URLSearchParams();
+  parámetros.append("id", doc.id);
   return ( /* html */
-    `<li class="fila">
-      <strong class="primario">
-        ${cod(data.nombre)}
-      </strong>
-      <span class="secundario">
-        ${cod(data.precio)}
-      </span>
+    `<li>
+      <a class="fila" href=
+  "pasatiempo.html?${parámetros}">
+        <strong class="primario">
+          ${nombre}
+        </strong>
+      </a>
     </li>`);
 }
 
-/** Función que se invoca cuando
- * hay un error al recuperar los
- * mensajes y muestra el error. Al
- * invocar esta función, la
- * conexión se cancela, por lo
- * cual intenta conectarse otra
- * vez.
- * @param {Error} e */
+/** @param {Error} e */
 function errConsulta(e) {
   muestraError(e);
-  // Intenta conectarse otra vez.
   consulta();
 }
