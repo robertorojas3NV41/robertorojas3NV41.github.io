c223ef4db7942cf15d4688cfb41c7d76f6363e5f
robertorojas3NV41 <86042594+robertorojas3NV41@users.noreply.github.com>
2021-06-19 19:09:55 -0500

    Update CtrlArticulos.js

--- js/CtrlArticulos.js
+++ js/CtrlArticulos.js
@@ -64,3 +64,104 @@ async function agrega(evt) {
     muestraError(e);
   }
 }
+
+/** Muestra los mensajes
+ * almacenados en la collection
+ * "Mensaje". Se actualiza
+ * automáticamente. */
+function consulta() {
+  /* Consulta que se actualiza
+   * automáticamente. Pide todos
+   * los registros de la colección
+   *  "Mensaje"
+   * ordenados por el campo
+   *  "timestamp"
+   * de forma
+   *  descendente. */
+  daoMensaje.
+    orderBy("timestamp", "desc").
+    onSnapshot(
+      htmlLista, errConsulta);
+}
+
+/** Muestra los datos enviados por
+ * el servidor.
+ * Si los datos cambian en el
+ * servidor, se vuelve a invocar
+ * esta función y recibe los datos
+ * actualizados.
+ * @param {import(
+    "../lib/tiposFire.js").
+    QuerySnapshot} snap estructura
+ *    parecida a un Array, que
+ *    contiene una copia de los
+ *    datos del servidor.
+ */
+function htmlLista(snap) {
+  let html = "";
+  if (snap.size > 0) {
+    /* Cuando el número de
+     * documentos devueltos por la
+     * consulta es mayor que 0,
+     * revisa uno por uno los
+     * documentos de la consulta y
+     * los muestra. El iterador
+     * "doc" apunta a un
+     * documento de la base
+     * de datos. */
+    snap.forEach(doc =>
+      html += htmlFila(doc));
+  } else {
+    /* Cuando el número de
+     * documentos devueltos por la
+     * consulta es igual a 0,
+     * agrega un texto HTML. */
+    html += /* html */
+      `<li class="vacio">
+        -- No hay mensajes
+        registrados. --
+      </li>`;
+  }
+  lista.innerHTML = html;
+}
+
+/** Agrega el texto HTML
+ * que corresponde a un
+ * documento de un mensaje.
+ * @param {import(
+    "../lib/tiposFire.js").
+    DocumentSnapshot} doc */
+function htmlFila(doc) {
+  /** Recupera los datos del
+   * documento.
+   * @type {import("./tipos.js").
+                      Mensaje} */
+  const data = doc.data();
+  /* Agrega un li con los datos
+   * del documento, los cuales se
+   * codifican para evitar
+   * inyección de código. */
+  return ( /* html */
+    `<li class="fila">
+      <strong class="primario">
+        ${cod(data.usuarioId)}
+      </strong>
+      <span class="secundario">
+        ${cod(data.texto)}
+      </span>
+    </li>`);
+}
+
+/** Función que se invoca cuando
+ * hay un error al recuperar los
+ * mensajes y muestra el error. Al
+ * invocar esta función, la
+ * conexión se cancela, por lo
+ * cual intenta conectarse otra
+ * vez.
+ * @param {Error} e */
+function errConsulta(e) {
+  muestraError(e);
+  // Intenta conectarse otra vez.
+  consulta();
+}
